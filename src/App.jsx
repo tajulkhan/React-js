9939178f3b76aaf1c5a52d1229c87e797a4c609f
tajulkhan <tajulbasheer@gmail.com>
2024-11-26 00:04:21 +0530

    css changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,19 @@ import Profile from './components/profile';
 import PackingList from './components/conditional-rendering.jsx';
 import RenderingList from './components/rendering-lists.jsx';
 import Counter from './components/counter.jsx';
+import Card from './components/state/card.jsx';
+import UseEffectExample from './components/effect/useEffectExample';
+import GitHubUser from './components/effect/GitHubUser';
 function App() {
   
   return (
     <>
+      <GitHubUser/>
+
+    <div className="">
+      <Card/>
+      <UseEffectExample/>
+    </div>
     <div className="">
       <Counter/>
     </div>
